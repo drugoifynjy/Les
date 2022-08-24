1246cecac392d0820532506b67cada228ff6f044
drugoifynjy <drugoifynjy@yandex.ru>
2022-08-24 15:45:20 +0600

    Добавил в базу модели организации и связаные сней данные

---
+++ les/organization/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+from .models import *
+
+
+@admin.register(OrganizationAddress)
+class OrganizationAddressAdmin(admin.ModelAdmin):
+    list_display = ('locality', 'street', 'house_number', 'apartment_number')
+
+
+@admin.register(RequisitesOrganization)
+class RequisitesOrganizationAdmin(admin.ModelAdmin):
+    list_display = ('INN', 'KPP', 'OGRN', 'OKPO', 'email')
+
+
+@admin.register(BankDetails)
+class BankDetailsAdmin(admin.ModelAdmin):
+    list_display = ('bank_title', 'bank_account', 'correspondent_account', 'BIK')
+
+
+@admin.register(OrganizationRepresentative)
+class OrganizationRepresentativeAdmin(admin.ModelAdmin):
+    list_display = ('second_name', 'first_name', 'patronymic', 'position')
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'organization_address', 'requisites_organization', 'bank_details')
---
+++ les/organization/models.py
@@ -0,0 +1,101 @@
+from django.db import models
+
+
+class OrganizationAddress(models.Model):
+    postcode = models.CharField(max_length=6, blank=True, null=True, verbose_name='Индекс', default='644001')
+    region = models.CharField(max_length=100, blank=True, null=True, verbose_name='Область', default='Омская')
+    district = models.CharField(max_length=50, blank=True, null=True, verbose_name='Район', default='')
+    locality = models.CharField(max_length=50, blank=True, null=True, verbose_name='Населенный пункт', default='г. Омск')
+    street = models.CharField(max_length=50, blank=True, null=True, verbose_name='Улица', default='ул. Куйбышева')
+    house_number = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name='Дом', default='63')
+    apartment_number = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name='Квартира', default='')
+
+    def __str__(self):
+        if self.apartment_number:
+            a = str(self.postcode.__str__()) + ' ' + \
+                str(self.region.__str__()) + ' область ' + \
+                str(self.locality.__str__()) + ' ул. ' + \
+                str(self.street.__str__()) + ' д. ' + \
+                str(self.house_number.__str__()) + ' кв. ' + \
+                str(self.apartment_number.__str__())
+        else:
+            a = str(self.postcode.__str__()) + ' ' + \
+                str(self.region.__str__()) + ' область ' + \
+                str(self.locality.__str__()) + ' ул. ' + \
+                str(self.street.__str__()) + ' д. ' + \
+                str(self.house_number.__str__())
+        return a
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Адрес организации'
+        verbose_name_plural = 'Адреса организации'
+
+
+class RequisitesOrganization(models.Model):
+    INN = models.CharField(max_length=10, blank=True, null=True, verbose_name='ИНН', default='')
+    KPP = models.CharField(max_length=9, blank=True, null=True, verbose_name='КПП', default='')
+    OGRN = models.CharField(max_length=13, blank=True, null=True, verbose_name='ОГРН', default='')
+    OKPO = models.CharField(max_length=8, blank=True, null=True, verbose_name='ОКПО', default='')
+    email = models.CharField(max_length=50, blank=True, null=True, verbose_name='адрес электронной почты', default='')
+
+    def __str__(self):
+        a = 'ИНН ' + str(self.INN) + ' ОГРН ' + str(self.OGRN)
+        return a
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Реквизиты организации'
+        verbose_name_plural = 'Реквизиты организаций'
+
+
+class BankDetails(models.Model):
+    bank_title = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Название банка',
+                            default='ОТДЕЛЕНИЕ ОМСКБАНКА РОССИИ // УФК по Омской области г.Омск')
+    bank_account = models.CharField(max_length=20, blank=True, null=True, verbose_name='Расчетный счет', default='')
+    correspondent_account = models.CharField(max_length=20, blank=True, null=True, verbose_name='Корсчет', default='')
+    BIK = models.CharField(max_length=9, blank=True, null=True, verbose_name='БИК', default='')
+
+    def __str__(self):
+        a = str(self.bank_title)+' '+str(self.bank_account)
+        return a
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Банковские реквизиты организации'
+        verbose_name_plural = 'Банковские реквизиты организаций'
+
+
+class OrganizationRepresentative(models.Model):
+    second_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    patronymic = models.CharField(max_length=50, blank=True, null=True, verbose_name='Отчество')
+    position = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Должность')
+
+    def __str__(self):
+        a = str(self.position)+' '+str(self.second_name)+' '+str(self.first_name)+' '+str(self.patronymic)+' '+str(self.position)
+        return a
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Представитель организации'
+        verbose_name_plural = 'Представители организаций'
+
+class Organization(models.Model):
+    title = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Название организации',
+                             default='Главное управление лесного хозяйства')
+    organization_address = models.OneToOneField(OrganizationAddress, on_delete=models.CASCADE,
+                                             blank=True, null=True, verbose_name='Адрес организации')
+    requisites_organization = models.OneToOneField(RequisitesOrganization, on_delete=models.CASCADE,
+                                             blank=True, null=True, verbose_name='Реквизиты организации')
+    bank_details = models.OneToOneField(BankDetails, on_delete=models.CASCADE,
+                                             blank=True, null=True, verbose_name='Банковские реквизиты')
+    organization_representative = models.ForeignKey(OrganizationRepresentative, blank=True, null=True, on_delete=models.CASCADE, verbose_name='Представитель организации')
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Организация'
+        verbose_name_plural = 'Организации'
+
+
+
