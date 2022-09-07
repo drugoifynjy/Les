8310c5bbaeb302c6706874c87132d12be3ca2da3
fynjy <drugoifynjy@yandex.ru>
2022-09-07 15:02:41 +0600

    еще не работает

--- les/organization/urls.py
+++ les/organization/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-
-from person.views import LoginUser
 from .views import *
 
 urlpatterns = [
@@ -10,6 +8,4 @@ urlpatterns = [
     path('guides/organizations/<int:org_pk>/representative_mod/<int:pk>', RepresentativeAddOrMod.as_view(), name='representative_mod'),
     path('guides/organizations/<int:org_pk>/bank_details_list/', BankDetailsList.as_view(), name='bank_details_list'),
     path('guides/organizations/<int:org_pk>/bank_details_mod/<int:pk>', BankDetailsAddOrMod.as_view(), name='bank_details_mod'),
-
-    #path('guides/organizations/representative_add/', RepresentativeAddOrMod.as_view(), name='representative_add'),
 ]
